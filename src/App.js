f945720e661aac3e56c23c47490b7066d5dc7ff3
James Bamberger <0xmesaj@protonmail.com>
2022-10-24 18:18:59 -0400

    update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Mining App
+          Mining App Dashboard
         </p>
       </header>
       <MachineData/>
